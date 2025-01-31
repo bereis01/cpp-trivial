d7fce11be4bfa7d046d1a7f7ea2cd3d23a8e9806
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2025-01-31 19:20:49 -0300

    Changed code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int wrong_vector_access(const uint8_t *vector, size_t size)
 {
     int sum = 0;
-    for (size_t i = 0; i < size + 1; i++)
+    for (size_t i = 0; i < size; i++)
     {
         sum += vector[i];
     }
