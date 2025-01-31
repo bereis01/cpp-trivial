344d6f6ee34212e3e97f450be75917311686521c
Bernardo Reis <bernardo.reis.almeida01@gmail.com>
2025-01-31 19:24:26 -0300

    Changed code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "main.h"
 
-int wrong_vector_access(const uint8_t *vector, size_t size)
+void wrong_vector_access(const uint8_t *vector, size_t size)
 {
-    int sum = 0;
-    for (size_t i = 0; i < size; i++)
+    int i = 0;
+    while (true)
     {
-        sum += vector[i];
-    }
+        if (vector[i] == size || vector[i] == 0)
+        {
+            break;
+        }
 
-    return sum;
+        i++;
+    }
 }
--- main.h
+++ main.h
@@ -1,3 +1,3 @@
 #include <iostream>
 
-int wrong_vector_access(const uint8_t *vector, size_t size);
+void wrong_vector_access(const uint8_t *vector, size_t size)
